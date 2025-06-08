60303e5050c26544dd397fbdf4f8a220b22ec9c6
jhs512 <jangka512@gmail.com>
2025-06-08 17:28:43 +0900

    work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 print("== 명언 앱 ==")
 
+last_id = 0
+
 while True:
     print("명언) ", end="")
     cmd = input()
@@ -12,5 +14,6 @@ while True:
         content = input()
         print("작가 : ", end="")
         author = input()
-        id = 1
+        last_id += 1
+        id = last_id
         print(f"{id}번 명언이 등록되었습니다.")
