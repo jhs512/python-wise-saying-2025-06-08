381fc1be7ff67b5825ebbc1565ee0d87ca061fda
jhs512 <jangka512@gmail.com>
2025-06-08 16:43:39 +0900

    work

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ while True:
     if cmd == "종료":
         print("명언 앱을 종료합니다.")
         break
+    elif cmd == "등록":
+        print("명언 : ", end="")
+        content = input()
+        print("작가 : ", end="")
+        author = input()
