30ac50cfcf29764407fc577328c349e6c2f87d3c
jhs512 <jangka512@gmail.com>
2025-06-08 17:35:33 +0900

    work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("== 명언 앱 ==")
 
 last_id = 0
+wise_sayings = []
 
 while True:
     print("명언) ", end="")
@@ -16,4 +17,19 @@ while True:
         author = input()
         last_id += 1
         id = last_id
+
+        wise_saying = {
+            "id": id,
+            "content": content,
+            "author": author
+        }
+
+        wise_sayings.append(wise_saying)
+
         print(f"{id}번 명언이 등록되었습니다.")
+    elif cmd == "목록":
+        print("번호 / 작가 / 명언")
+        print("----------------------")
+
+        for wise_saying in wise_sayings:
+            print(f"{wise_saying['id']} / {wise_saying['author']} / {wise_saying['content']}")
